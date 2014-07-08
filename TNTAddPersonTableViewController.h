24361a8b47fd40ce9bc95ec4a83e2c0e66a424e4
JoeSelvik <joe@thenewtricks.com>
2014-07-07 23:34:25 -0600

    adding add person TVVC

---
+++ TNTAddPersonTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TNTAddPersonTableViewController.h
+//  InlineDatePicker
+//
+//  Created by Joe Selvik on 7/7/14.
+//  Copyright (c) 2014 The New Tricks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TNTAddPersonTableViewController : UITableViewController
+
+@end
